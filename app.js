ff12b9d414d2c4ca68f230f7b518db98c24309a6
BiancaDullius <bianca.dullius@paipe.co>
2023-05-10 18:49:49 -0300

    ajustes

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const helmet = require("helmet");
 const app = express(); // create an instance of express
 const Knex = require("knex"); // SQL query builder
 const cors = require('cors');
-/* var PORT = 4000; */
-/* var http = require("http");
-var https = require("https"); */
 
-/* app.listen(PORT, function (err) {
-  if (err) console.log("Error in server setup");
-  console.log("Server listening on Port", PORT);
-}); */
 app.use(cors({
   methods: ['GET','PATCH'],
   origin: '*'
@@ -29,18 +22,7 @@ const connect = () => {
       searchPath: ['knex', 'public'],
     });
   };
-// const connect = () => {
-//   return Knex({
-//     client: "pg",
-//     connection: {
-//       host: "trabalho-pbd.postgres.database.azure.com",
-//       port: 5432,
-//       user: "BiancaBeppler",
-//       password: "Tr@balhoPBD",
-//       database: "postgres",
-//     },
-//   });
-// };
+
 exports.knex = connect();
 
 const indexRoute = require("./src/routes/index-route");
@@ -48,9 +30,4 @@ const indexRoute = require("./src/routes/index-route");
 app.use("/", indexRoute);
 
 module.exports = app;
-/* var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
- */
